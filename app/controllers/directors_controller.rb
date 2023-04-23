20f256fd4f043cf5d358f61948ad0a8c5edf9e73
Finch Yuan <fyuan@chicagobooth.edu>
2023-04-23 18:46:33 +0000

    Index and order Finished

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,8 +1,17 @@
 class DirectorsController < ApplicationController
 
   def index
-
     render({ :template =>"directors_templates/index.html.erb"})
   end
-end
 
+  def eldest
+    @eldest = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
+    render({ :template =>"directors_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest = Director.where.not({:dob => nil}).order({:dob => :desc}).at(0)
+    render({ :template =>"directors_templates/youngest.html.erb"})
+  end
+
+end
